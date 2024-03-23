5ad7b3f86950ad7e3bc1a0f301acfc6c5dca96da
PikaLuba <157685096+PikaLuba@users.noreply.github.com>
2024-03-23 20:42:09 +0200

    Update CurrencyBotConstance.java

--- currency_bot/src/main/java/com/example/CurrencyBotConstance.java
+++ currency_bot/src/main/java/com/example/CurrencyBotConstance.java
@@ -1,8 +1,8 @@
 package com.example;
 
 public class CurrencyBotConstance {
-    public static final String BOT_NAME = "GoITJavaLPCurrencyBot";
-    public static final String BOT_TOKEN = "6165759381:AAFjcxzg9JkRrmDAsN-ixe1TxqkmVjHmyeo";
+    public static final String BOT_NAME = "jj";
+    public static final String BOT_TOKEN = "jjjj";
 
     public static final String EMOJI_CHECKED = ":white_check_mark:";
     public static final String EMOJI_SETTINGS = ":gear:";
