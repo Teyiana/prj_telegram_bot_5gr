c9adfdb69ceee6737e6e8b1a3a5ffd02caa96940
Tetiana <tytyshkamalyshka@gmail.com>
2024-03-24 21:29:05 +0200

    get info

--- currency_bot/src/main/java/com/example/commands/GetInfoCommand.java
+++ currency_bot/src/main/java/com/example/commands/GetInfoCommand.java
@@ -1,10 +1,16 @@
 package com.example.commands;
 
 import com.example.configuration.ChatConfig;
+import com.example.currencyPackage.BankService;
+import com.example.currencyPackage.CurrencyRates;
+import com.example.ui.PrettyCurrencyService;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,8 +25,29 @@ public class GetInfoCommand implements SendCommand {
     @Override
     public BotApiMethod<?> execute(ChatConfig config, String message, Integer messageId) {
         SendMessage.SendMessageBuilder builder = createSendMethodBuilder(config.getChatId());
-        builder.text("Тут має бути текст з рейтами");
+        builder.text(getCurrencyRateText(config));
         return builder.build();
+    }
+
+    private String getCurrencyRateText(ChatConfig config) {
+        BankService service = BankService.getInstance();
+        List<CurrencyRates> rates = service.getCurrencyRates(Collections.singletonList(config)).get(config);
+        StringBuilder sb = new StringBuilder();
+        sb.append("Курс в ");
+        sb.append(config.getSelectedBank().getNameBank());
+        for (CurrencyRates rate: rates) {
+            sb.append("\n").append(rate.getCurrency().getValue()).append("\\UAH\n");
+            sb.append("Купівля ").append(roundValue(rate.getBuyRate(), config.getDigitCount())).append("\n");
+            sb.append("Продаж ").append(roundValue(rate.getSellRate(), config.getDigitCount())).append("\n");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+
+    private String roundValue(double rate, int digits){
+        BigDecimal r = BigDecimal.valueOf(rate);
+        r = r.setScale(digits, RoundingMode.HALF_UP);
+        return r.toPlainString();
     }
 
     @Override
