2003c59ed97e277bc0d69ccfae0011a948d1cc53
Tetiana <tytyshkamalyshka@gmail.com>
2024-03-24 14:32:22 +0200

    Revert "add test getinfo"
    This reverts commit 2b129f5b0bea3df7c8c3f4b6a0de9d140dfeb043.
    
    # Conflicts:
    #	currency_bot/src/main/java/com/example/ui/PrettyCurrencyService.java

--- currency_bot/src/main/java/com/example/CurrencyBotService.java
+++ currency_bot/src/main/java/com/example/CurrencyBotService.java
@@ -2,12 +2,11 @@ package com.example;
 
 import com.example.commands.Command;
 import com.example.commands.CommandRegistry;
-import com.example.currencyPackage.*;
-import com.example.ui.PrettyCurrencyService;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 import static com.example.CurrencyBotConstance.BOT_NAME;
 import static com.example.CurrencyBotConstance.BOT_TOKEN;
 
@@ -15,13 +14,6 @@ public class CurrencyBotService extends TelegramLongPollingBot {
 
     private static final String COMMAND_NOT_FOUND = "Вибачте, команду не знайдено!\nСпробуйте знову";
     private CommandRegistry commandRegistry;
-
-    private static int bankId;
-    private  static int prettyId;
-    private static String cash;
-    private CurrencyService currencyService;
-    private PrettyCurrencyService prettyCurrencyService;
-
     public CurrencyBotService() {
         super(BOT_TOKEN);
         commandRegistry = CommandRegistry.getRegistry();
@@ -37,16 +29,6 @@ public class CurrencyBotService extends TelegramLongPollingBot {
     }
 
     private void processMessage(Update update, String chatId, String message) {
-
-        SendMessage responseMessage = new SendMessage();
-
-        if (message.equals("/start")) {
-            bankId = 1;   // значення за замовчуванням
-            prettyId = 3;  // значення за замовчуванням
-            cash = "USD";   // значення за замовчуванням
-
-        }
-
         Command command = commandRegistry.getCommand(message);
         SendMessage.SendMessageBuilder builder = SendMessage.builder();
         if (command != null) {
@@ -54,55 +36,13 @@ public class CurrencyBotService extends TelegramLongPollingBot {
         } else {
             builder.text(COMMAND_NOT_FOUND);
         }
-
-        if (message.equals("/getInfo")) {
-
-            if (bankId == 1) {
-                currencyService = new PrivateBankCurrencyService();
-            } else if (bankId == 2) {
-                currencyService = new NBUCurrencyService();
-            } else {
-                currencyService = new MonoCurrencyService();
-            }
-
-            prettyCurrencyService = new PrettyCurrencyService();
-
-            String callbackQuery = update.getCallbackQuery().getData();
-            CurrencyBank currency = CurrencyBank.valueOf(cash);
-
-            for (int i = 0; i <= 2; i++) {
-                double currencyRateBuy = 0.0;
-
-                if (i != 0) {
-                    currencyRateBuy = currencyService.getRateBuy(currency, i);
-                  //  System.out.println(currencyRateBuy);
-                }
-                String prettyText = prettyCurrencyService.convert(currencyRateBuy, currency, i, bankId, prettyId);
-
-                SendMessage resMessage = new SendMessage();
-                resMessage.setText(prettyText);
-                resMessage.setChatId(chatId);
-
-                try {
-                    execute(resMessage);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-
-                }
-
-            }
-        }
-
-        if (message.equals("/getInfo")) {
-            //
-        } else {
-            try {
-                execute(builder.build());
-            } catch (TelegramApiException e) {
-                throw new RuntimeException(e);
-            }
+        try {
+            execute(builder.build());
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
         }
     }
+
 
     @Override
     public String getBotUsername() {
--- currency_bot/src/main/java/com/example/configuration/ConfigManager.java
+++ currency_bot/src/main/java/com/example/configuration/ConfigManager.java
@@ -1,10 +1,5 @@
 package com.example.configuration;
 
-import com.example.currencyPackage.CurrencyBank;
-import com.example.currencyPackage.CurrencyService;
-import com.example.currencyPackage.PrivateBankCurrencyService;
-
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,32 +28,4 @@ public class ConfigManager {
         configCache = new HashMap<>();
         //TODO: load from json
     }
-
-
-    private void vvv() {
-       // String message = "/getInfo";
-
-     //   if (message.equals("/getInfo")) {
-
-            // if (bankId == 1) {
-            CurrencyService currencyService = new PrivateBankCurrencyService();
-            //   } else if (bankId == 2) {
-            //   CurrencyService currencyService = new NBUCurrencyService();
-            //   } else {
-            //    CurrencyService currencyService = new MonoCurrencyService();
-            //  }
-            String cash = "USD";
-            int prettyId = 3;
-
-            CurrencyBank currency = CurrencyBank.valueOf(cash);
-            for (int i = 1; i <= 2; i++) {
-                double currencyRate = currencyService.getRateBuy(currency, i);
-                BigDecimal roundedRate = new BigDecimal(currencyRate);
-                roundedRate = roundedRate.setScale(prettyId, BigDecimal.ROUND_DOWN);
-
-                System.out.println("USD= " + currencyRate + "=" + roundedRate);
-            }
-      //  }
-    }
-
 }
