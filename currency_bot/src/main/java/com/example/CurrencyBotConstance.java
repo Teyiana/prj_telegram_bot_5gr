5327ad264399262674728cba48056fd08cd14ea8
William <viliammomo@gmail.com>
2024-03-25 20:39:27 +0100

    final3

--- currency_bot/src/main/java/com/example/CurrencyBotConstance.java
+++ currency_bot/src/main/java/com/example/CurrencyBotConstance.java
@@ -1,9 +1,8 @@
 package com.example;
 
 public class CurrencyBotConstance {
-    public static final String BOT_NAME = "CurrencyGoITMomoBot";
-    public static final String BOT_TOKEN = "7055699904:AAE4ioJdIoCTUf6_xbt0foMEayna3fuZuq4";
-
+    public static final String BOT_NAME = "botname";
+    public static final String BOT_TOKEN = "token";
     public static final String EMOJI_CHECKED = "\u2705";
     public static final String EMOJI_SETTINGS = "\u2699\uFE0F";
     public static final String EMOJI_GET_INFO = "\u2139\uFE0F";
